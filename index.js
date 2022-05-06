91a0054ed57e0a4cc97c557da3c3648646f270e8
Lesley Labady <labadylesley@gmail.com>
2022-05-06 12:05:17 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num2 * num1
 
+const random = Math.floor(Math.random()* 10 ) + 1;
+
+const num3 = 3
+const num4 = 1
+const mod = num3 + num4
+
+const max = Math.max(12, 13, 15, 17, 20)
 // Write your code here
